b84ece23058516307270cdc670effdc884ec894d
prajwalfc <prajwal_k@live.com>
2016-11-06 11:22:06 -0600

    signup parameters to controller form index.html

--- src/main/java/controller/SignUpController.java
+++ src/main/java/controller/SignUpController.java
@@ -1,6 +1,9 @@
 package controller;
 
 import java.io.IOException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
 
 /**
  * Created by Prajwal on 11/4/2016.
@@ -8,11 +11,32 @@ import java.io.IOException;
 public class SignUpController extends javax.servlet.http.HttpServlet {
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
 
+        String firstName= request.getParameter("firstname");
+        System.out.println(firstName);
+        String lastName= request.getParameter("lastname");
+        System.out.println(lastName);
+        String username= request.getParameter("username");
+        System.out.println(username);
+        String email= request.getParameter("email");
+        System.out.println(email);
+        String password= request.getParameter("password");
+        System.out.println(password);
+
+
 }
 
-    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+    protected void doGet( HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        System.out.println(request.getParameter("firstname"));
+        String firstName= request.getParameter("firstname");
+        System.out.println(firstName);
+        String lastName= request.getParameter("lastname");
+        System.out.println(lastName);
+        String username= request.getParameter("username");
+        System.out.println(username);
+        String email= request.getParameter("email");
+        System.out.println(email);
+        String password= request.getParameter("password");
+        System.out.println(password);
 
     }
 }
